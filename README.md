6dd5f6f5106c047d1b02e035c46f0cee4054e92a
Tenshii <55290516+CuteTenshii@users.noreply.github.com>
2025-11-22 03:32:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# E2EE app
+
+This is an end-to-end encrypted messaging app. ATM this is just for having something to build, but feel free to contribute :)
+
+## Contributing
+
+See [CONTRIBUTING.md](CONTRIBUTING.md).
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use axum::{Router, routing::get};
+
+async fn hello() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(hello))
+    ;
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
