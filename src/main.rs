048f8381aef61b2cb7df791a77128eec5fc96b70
Tenshii <55290516+CuteTenshii@users.noreply.github.com>
2025-11-23 03:36:06 +0100

    feat: add logging

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod routes;
 use axum::{Router, routing::get, Extension};
 use diesel::{PgConnection, r2d2::{self, ConnectionManager}};
 use dotenvy::dotenv;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber;
 
 type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
@@ -12,8 +14,6 @@ pub struct AppState {
 }
 
 fn establish_connection() -> DbPool {
-    dotenv().ok();
-
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     let pool = r2d2::Pool::builder()
@@ -25,13 +25,20 @@ fn establish_connection() -> DbPool {
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    tracing_subscriber::fmt()
+        .with_env_filter("info,tower_http=debug")
+        .init();
     let pool = establish_connection();
     let state = AppState { db: pool };
     let app = Router::new()
         .route("/messages", get(routes::messages::get_messages))
+        .layer(TraceLayer::new_for_http())
         .layer(Extension(state))
     ;
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("[Server] Started on http://0.0.0.0:3000");
     axum::serve(listener, app).await.unwrap();
 }
